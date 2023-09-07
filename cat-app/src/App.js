3597f03889234e14036182c230ab137d37bdac3b
Orellana Argueta <eorellana96@csu.fullerton.edu>
2023-09-07 14:54:55 -0700

    created react app and modified the website a bit

---
+++ cat-app/src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://bestanimations.com/Animals/Mammals/Cats/cats/cute-kitty-animated-gif-12.gif" alt="cat sleeping gif"
+            height="200px" width="200px"/>
+        
+        <h1>Cat Breed Recommendation App</h1>
+        <p>Which Cat Breed Is Best For You?</p>
+        <p class="summary">Take our quiz to find out which cat is sutible for you!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
